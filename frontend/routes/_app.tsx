5517c5eaaf60fdf1e4281a6fbaf116321b4ab8fa
Fredrik Hagen Fasteraune <frha@dips.no>
2026-06-05 10:01:41 +0200

    fix favico

--- frontend/routes/_app.tsx
+++ frontend/routes/_app.tsx
@@ -6,6 +6,8 @@ export default define.page(function App({ Component, state }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
         <title>{state.title}</title>
       </head>
       <body class="bg-sumi-ink1 text-fuji-white min-h-screen">
